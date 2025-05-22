d91142abb416310e348f602b297848c4bcdd6f06
Melsantoss <escolarmel@gmail.com>
2025-05-22 11:19:40 -0300

    aqui eu vi let, console e fiz uma calculadora

--- logicacomjs.js
+++ logicacomjs.js
@@ -1,1 +1,21 @@
-console.log ("OI")
+/*let number1 = 5;
+let number2 = 5;
+let som = number1+number2
+console.log(" A soma é: " +som)
+console.log ("------------------")
+let sub = number1-number2
+console.log("A subtração é: " +sub)
+console.log ("------------------")
+let mult = number1*number2
+console.log("A multiplicação é: " +mult)
+console.log ("------------------")
+let div = number1/number2
+console.log("A divisão é: " +div)
+console.log ("------------------")*/
+
+/*let salario = 3000
+let mesestrabalhados = 10
+let nome = "Antônio"
+let result = salario/12*mesestrabalhados
+console.log("Parabéns " + nome + " seu 13° salário é: R$ " + result )*/
+
